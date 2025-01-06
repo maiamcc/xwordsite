f1e5ec988b17fbcb88939fca4f84e82dbd96f52a
Maia <maia.mcc@gmail.com>
2025-01-05 22:39:24 -0500

    tweaks

--- assets/javascripts/hide_puzzles.js
+++ assets/javascripts/hide_puzzles.js
@@ -1,7 +1,6 @@
 // If url params includes ?hidden=anything (presumably ?hidden=true)
 //   but it doesn't actually matter, display hidden puzzles
 
-
 $(function() {
 	let urlParams = new URLSearchParams(window.location.search);
 	let hidden = urlParams.get('hidden');
